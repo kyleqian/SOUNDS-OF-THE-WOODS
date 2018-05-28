0aa8b3ce745c9c8edb8c4c9113dfd716ebc4754c
Kyle Qian <kylecqian@gmail.com>
2018-05-28 02:03:18 -0700

    Added test scene with Go-compatible flashlight

---
+++ Assets/_SCRIPTS/Flashlight.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Flashlight : MonoBehaviour
+{
+	[SerializeField] Light light;
+
+	void Update()
+	{
+		if (OVRInput.GetDown(OVRInput.Button.One))
+		{
+			light.enabled = !light.enabled;
+		}
+	}
+}
