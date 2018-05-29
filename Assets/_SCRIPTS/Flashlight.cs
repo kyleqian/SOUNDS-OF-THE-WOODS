e0ff3fcf0d80f5d207a506e01e85d5f9a03cd216
Kyle Qian <kylecqian@gmail.com>
2018-05-28 20:56:15 -0700

    GameManager added

--- Assets/_SCRIPTS/Flashlight.cs
+++ Assets/_SCRIPTS/Flashlight.cs
@@ -4,11 +4,27 @@ public class Flashlight : MonoBehaviour
 {
 	[SerializeField] Light light;
 
+	public float currBattery;
+
 	void Update()
 	{
+		if (currBattery <= 0)
+		{
+			return;
+		}
+
 		if (OVRInput.GetDown(OVRInput.Button.One))
 		{
 			light.enabled = !light.enabled;
+		}
+
+		if (light.enabled)
+		{
+			currBattery--;
+			if (currBattery <= 0)
+			{
+				light.enabled = false;
+			}
 		}
 	}
 }
---
+++ Assets/_SCRIPTS/GameManager.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+	[SerializeField] float minPhaseLengthInSeconds;
+	[SerializeField] float maxPhaseLengthInSeconds;
+
+	public static GameManager instance;
+	public enum GamePhase { Afternoon, Dusk, Night, Latenight, Dawn, End }
+	public GamePhase currPhase;
+	public float currPhaseTime;
+
+	public event Action<GamePhase> OnPhaseLoad;
+	public event Action<GamePhase> OnPhaseUnload;
+
+	float[] phaseLengths;
+
+	void Awake()
+	{
+		instance = this;
+
+		for (int i = 0; i < Enum.GetNames(typeof(GamePhase)).Length; ++i)
+		{
+			phaseLengths[i] = UnityEngine.Random.Range(minPhaseLengthInSeconds, maxPhaseLengthInSeconds);
+		}
+
+		// No limit for .End
+		phaseLengths[phaseLengths.Length - 1] = Mathf.Infinity;
+	}
+
+	void Update()
+	{
+		// Advance time
+		currPhaseTime += Time.deltaTime;
+		if (currPhaseTime >= phaseLengths[(int)currPhase])
+		{
+			PhaseTransition();
+		}
+	}
+
+	void PhaseTransition()
+	{
+		currPhaseTime = 0;
+		OnPhaseUnload(currPhase);
+		OnPhaseLoad(++currPhase);
+
+		switch (currPhase)
+		{
+			case GamePhase.Afternoon:
+				break;
+			case GamePhase.Dusk:
+				break;
+			case GamePhase.Night:
+				break;
+			case GamePhase.Latenight:
+				break;
+			case GamePhase.Dawn:
+				break;
+			case GamePhase.End:
+				break;
+		}
+	}
+}
