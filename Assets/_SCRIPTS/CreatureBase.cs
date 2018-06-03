3244c683bb633406ef106970726a15c2cc6f982a
Kyle Qian <kylecqian@gmail.com>
2018-06-02 18:34:54 -0700

    Creature code

---
+++ Assets/_SCRIPTS/CreatureBase.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+public enum CreatureState
+{
+    Default, Shocked, Fleeing
+}
+
+public abstract class CreatureBase : MonoBehaviour
+{
+    protected const float SECONDS_TO_SHOCK = 0.5f;
+    protected const float SECONDS_TO_FLEE = 2f;
+    protected CreatureState currState;
+
+    public virtual void Spawn(Vector3 location)
+    {
+        // Initialize location
+        transform.position = location;
+
+        // Change state to Default
+        ChangeState(CreatureState.Default);
+
+        // Enable GameObject
+        gameObject.SetActive(true);
+
+        // Fade in
+        // TODO
+    }
+
+    public virtual void Despawn()
+    {
+        // Fade out
+        // TODO
+
+        // Disable GameObject when faded out
+        // TODO
+    }
+
+    // Called by flashlight with value indicating how long
+    // the creature has been continuously looked at
+    public virtual void ISeeYou(float forThisLong)
+    {
+        // Change state
+        if (forThisLong >= SECONDS_TO_FLEE)
+        {
+            if (currState == CreatureState.Shocked)
+            {
+                ChangeState(CreatureState.Fleeing);
+            }
+        }
+        else if (forThisLong >= SECONDS_TO_SHOCK)
+        {
+            if (currState == CreatureState.Default)
+            {
+                ChangeState(CreatureState.Shocked);
+            }
+        }
+    }
+
+    protected abstract void ChangeState(CreatureState state);
+}
---
+++ Assets/_SCRIPTS/Friendly.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class Friendly : CreatureBase
+{
+    protected override void ChangeState(CreatureState state)
+    {
+        currState = state;
+        switch (state)
+        {
+            case CreatureState.Default:
+                // Play animation
+
+                // Move?
+
+                break;
+            case CreatureState.Shocked:
+                // Play animation
+                
+                // Stop moving
+                
+                break;
+            case CreatureState.Fleeing:
+                // Play animation
+
+                // Move?
+
+                break;
+        }
+    }
+}
