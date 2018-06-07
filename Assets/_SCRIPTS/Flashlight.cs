1b2775e25232d2f8d1db762d515ef8ed5dd76430
zephyo <heangela.arts@gmail.com>
2018-06-06 18:51:44 -0700

    flashlight

--- Assets/_SCRIPTS/Flashlight.cs
+++ Assets/_SCRIPTS/Flashlight.cs
@@ -7,17 +7,25 @@ public class Flashlight : MonoBehaviour
     [SerializeField] string layerMaskName;
     int layerMask;
 
+    public float MaxBattery = 10000;
+
+    public SpriteRenderer sprite;
+
+    Color green, red;
+
     void Awake()
     {
+        green = new Color(0.51f, 1, 0.25f);
+        red = new Color(0.89f, 0, 0.09f);
         layerMask = LayerMask.NameToLayer(layerMaskName);
     }
 
     void Update()
-	{
+    {
         HandleInputs();
         DrainBattery();
         DetectCreatures();
-	}
+    }
 
     void HandleInputs()
     {
@@ -41,7 +49,18 @@ public class Flashlight : MonoBehaviour
             {
                 lightSource.enabled = false;
             }
+            else
+            {
+                setSprite();
+            }
+
         }
+    }
+
+    void setSprite()
+    {
+        sprite.size = new Vector2(0.1f, Mathf.Lerp(0, 0.3f, CurrBattery / MaxBattery));
+        sprite.color = Color.Lerp(red, green, CurrBattery / MaxBattery);
     }
 
     void DetectCreatures()
